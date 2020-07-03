be9cf630d878eb717222a45a00eb5f9b7223e882
jerson100 <juamkoo@gmail.com>
2020-07-02 19:37:16 -0500

    Se realizó el slider

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 ((window,document,undefined)=>{
-    
+    document.addEventListener('DOMContentLoaded',evt=>{
+        const sliderInner = document.querySelector(".slider__inner");
+        const sliderNav = document.querySelector(".slider__nav-list");
+        const items =  Array.from(document.querySelectorAll(".slider__nav-li"));
+        sliderNav.addEventListener('click',e=>{
+            if(e.target.classList.contains("slider__nav-li")){
+                let itemSelect = e.target.dataset.li;
+                clearSelection();
+                e.target.classList.add("slider__nav-li--selected");
+                sliderInner.style.transform = `translateX(-${itemSelect * 100}%)`;
+            }
+        });
+        const clearSelection = () => {
+            console.log(items);
+            items.forEach(item=>{
+                item.classList.remove("slider__nav-li--selected");
+            });
+        };
+    });
+
+   
 })(window,document);
